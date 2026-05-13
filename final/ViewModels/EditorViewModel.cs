23bca527fa538a01f99175acf4d3379ce21d927c
zipzk251amm-web <zipzk251_amm@student.ztu.edu.ua>
2026-05-13 14:16:07 +0300

    refactor: використано RecipeFactory у EditorViewModel

--- final/ViewModels/EditorViewModel.cs
+++ final/ViewModels/EditorViewModel.cs
@@ -1,27 +1,46 @@
-public class EditorViewModel : BaseViewModel
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using final;
+
+namespace final.ViewModels
 {
-	private Recipe _editingRecipe;
-	public Recipe EditingRecipe
-	{
-		get => _editingRecipe;
-		set => SetProperty(ref _editingRecipe, value);
-	}
+    public class EditorViewModel : BaseViewModel
+    {
+        private Recipe _editingRecipe;
 
-	public ICommand SaveCommand { get; }
-	public ICommand AddIngredientCommand { get; }
+        public Recipe EditingRecipe
+        {
+            get => _editingRecipe;
+            set => SetProperty(ref _editingRecipe, value);
+        }
 
-	public EditorViewModel(Recipe recipe = null)
-	{
-		EditingRecipe = recipe ?? new Recipe();
-		SaveCommand = new RelayCommand(obj => Save(), can => IsValid());
-	}
+        public ICommand SaveCommand { get; }
+        public ICommand AddIngredientCommand { get; }
 
-	private bool IsValid()
-	{
-		
-		return !string.IsNullOrWhiteSpace(EditingRecipe.Title) &&
-			   EditingRecipe.Ingredients.Count > 0;
-	}
+        public EditorViewModel(Recipe recipe = null)
+        {
+            if (recipe != null)
+            {
+                EditingRecipe = recipe;
+            }
+            else
+            {
+                var factory = new RecipeFactory();
+                EditingRecipe = factory.CreateRecipe("", new List<Ingredient>(), new List<string>());
+            }
+            SaveCommand = new RelayCommand(obj => Save(), can => IsValid());
+        }
 
-	private void Save() {  }
+        private bool IsValid()
+        {
+            return !string.IsNullOrWhiteSpace(EditingRecipe.Title) &&
+                   EditingRecipe.Ingredients.Count > 0;
+        }
+
+        private void Save()
+        {
+            // Логіка збереження рецепта
+        }
+    }
 }
